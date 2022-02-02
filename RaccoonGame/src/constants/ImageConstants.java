f88fc3667d90d8eb0c960d4830717f7648d0d23f
Cheon Mungi <mongi7421@gmail.com>
2022-02-03 01:56:15 +0900

    Add - 해상도에 따라 이미지 리사이징 기능 추가, 이미지 경로 관리 클래스 추가

---
+++ RaccoonGame/src/constants/ImageConstants.java
@@ -0,0 +1,144 @@
+package constants;
+
+/**
+ * 게임 스테이지, 캐릭터, 오브젝트 이미지 위치를 관리하는 클래스
+ * @author CheonMungi
+ *
+ */
+public class ImageConstants
+{
+
+	/************************/
+	/********너구리 캐릭터********/
+	/************************/
+	/** 너구리 : 왼쪽*/
+	public static String RACCON_LEFT_SPRITES = "src/image/raccoon/CharterLeftSprite.png";
+	/** 너구리 : 오른쪽*/
+	public static String RACCON_RIGHT_SPRITES = "src/image/raccoon/CharterRightSprite.png";
+	/** 너구리 : 추락*/
+	public static String RACCON_FALL_SPRITES = "src/image/raccoon/fallSprite.png";
+	/** 너구리 : 사다리타기*/
+	public static String RACCON_LADDER_SPRITES = "src/image/raccoon/LadderSprite.png";
+	/** 너구리 : 왼쪽 점프 1*/
+	public static String RACCON_LEFT_JUMP1 = "src/image/raccoon/leftJump1.png";
+	/** 너구리 : 왼쪽 점프 2*/
+	public static String RACCON_LEFT_JUMP2 = "src/image/raccoon/leftJump2.png";
+	/** 너구리 : 오른쪽 점프 1*/
+	public static String RACCON_RIGHT_JUMP1 = "src/image/raccoon/rightJump1.png";
+	/** 너구리 : 오른쪽 점프 2*/
+	public static String RACCON_RIGHT_JUMP2 = "src/image/raccoon/rightJump2.png";
+
+	/**********************/
+	/********적캐릭터********/
+	/**********************/
+	/** 적 캐릭터 : 초록 지네 (오른쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_RIGHT_GREEN_CENTIPEDE_DOWN = "src/image/enemy/rightCentipede_green.png";
+	/** 적 캐릭터 : 초록 지네 (오른쪽 방향 높은 꼬리)*/
+	public static String ENEMY_RIGHT_GREEN_CENTIPEDE_UP = "src/image/enemy/rightCentipede2_green.png";
+	/** 적 캐릭터 : 초록 지네 (왼쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_LEFT_GREEN_CENTIPEDE_DOWN = "src/image/enemy/leftCentipede_green.png";
+	/** 적 캐릭터 : 초록 지네 (왼쪽 방향 높은 꼬리)*/
+	public static String ENEMY_LEFT_GREEN_CENTIPEDE_UP = "src/image/enemy/leftCentipede2_green.png";
+
+	/** 적 캐릭터 : 주황 지네 (오른쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_RIGHT_ORANGE_CENTIPEDE_DOWN = "src/image/enemy/rightCentipede_orange.png";
+	/** 적 캐릭터 : 주황 지네 (오른쪽 방향 높은 꼬리)*/
+	public static String ENEMY_RIGHT_ORANGE_CENTIPEDE_UP = "src/image/enemy/rightCentipede2_orange.png";
+	/** 적 캐릭터 : 주황 지네 (왼쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_LEFT_ORANGE_CENTIPEDE_DOWN = "src/image/enemy/leftCentipede_orange.png";
+	/** 적 캐릭터 : 주황 지네 (왼쪽 방향 높은 꼬리)*/
+	public static String ENEMY_LEFT_ORANGE_CENTIPEDE_UP = "src/image/enemy/leftCentipede2_orange.png";
+
+	/** 적 캐릭터 : 빨강 지네 (오른쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_RIGHT_RED_CENTIPEDE_DOWN = "src/image/enemy/rightCentipede_red.png";
+	/** 적 캐릭터 : 빨강 지네 (오른쪽 방향 높은 꼬리)*/
+	public static String ENEMY_RIGHT_RED_CENTIPEDE_UP = "src/image/enemy/rightCentipede2_red.png";
+	/** 적 캐릭터 : 빨강 지네 (왼쪽 방향 낮은 꼬리)*/
+	public static String ENEMY_LEFT_RED_CENTIPEDE_DOWN = "src/image/enemy/leftCentipede_red.png";
+	/** 적 캐릭터 : 빨강 지네 (왼쪽 방향 높은 꼬리)*/
+	public static String ENEMY_LEFT_RED_CENTIPEDE_UP = "src/image/enemy/leftCentipede2_red.png";
+
+	/** 적 캐릭터 : 초록 뱀 (오른쪽 방향 낮은 혀)*/
+	public static String ENEMY_RIGHT_GREEN_SNAKE_DOWN = "src/image/enemy/rightSnake2_green.png";
+	/** 적 캐릭터 : 초록 뱀 (오른쪽 방향 높은 혀)*/
+	public static String ENEMY_RIGHT_GREEN_SNAKE_UP = "src/image/enemy/rightSnake_green.png";
+	/** 적 캐릭터 : 초록 뱀 (왼쪽 방향 낮은 혀)*/
+	public static String ENEMY_LEFT_GREEN_SNAKE_DOWN = "src/image/enemy/leftSnake2_green.png";
+	/** 적 캐릭터 : 초록 뱀 (왼쪽 방향 높은 혀)*/
+	public static String ENEMY_LEFT_GREEN_SNAKE_UP = "src/image/enemy/leftSnake_green.png";
+
+	/** 적 캐릭터 : 주황 뱀 (오른쪽 방향 낮은 혀)*/
+	public static String ENEMY_RIGHT_ORANGE_SNAKE_DOWN = "src/image/enemy/rightSnake2_orange.png";
+	/** 적 캐릭터 : 주황 뱀 (오른쪽 방향 높은 혀)*/
+	public static String ENEMY_RIGHT_ORANGE_SNAKE_UP = "src/image/enemy/rightSnake_orange.png";
+	/** 적 캐릭터 : 주황 뱀 (왼쪽 방향 낮은 혀)*/
+	public static String ENEMY_LEFT_ORANGE_SNAKE_DOWN = "src/image/enemy/leftSnake2_orange.png";
+	/** 적 캐릭터 : 주황 뱀 (왼쪽 방향 높은 혀)*/
+	public static String ENEMY_LEFT_ORANGE_SNAKE_UP = "src/image/enemy/leftSnake_orange.png";
+
+	/** 적 캐릭터 : 빨강 뱀 (오른쪽 방향 낮은 혀)*/
+	public static String ENEMY_RIGHT_RED_SNAKE_DOWN = "src/image/enemy/rightSnake2_red.png";
+	/** 적 캐릭터 : 빨강 뱀 (오른쪽 방향 높은 혀)*/
+	public static String ENEMY_RIGHT_RED_SNAKE_UP = "src/image/enemy/rightSnake_red.png";
+	/** 적 캐릭터 : 빨강 뱀 (왼쪽 방향 낮은 혀)*/
+	public static String ENEMY_LEFT_RED_SNAKE_DOWN = "src/image/enemy/leftSnake2_red.png";
+	/** 적 캐릭터 : 빨강 뱀 (왼쪽 방향 높은 혀)*/
+	public static String ENEMY_LEFT_RED_SNAKE_UP = "src/image/enemy/leftSnake_red.png";
+
+	/**********************/
+	/********아이템류********/
+	/**********************/
+	/** 적 오브젝트 : 압정*/
+	public static String ENEMY_GIMLET = "src/image/item/gimlet.png";
+	/** 오브젝트 : 랜덤 주머니*/
+	public static String OBJECT_SCRETITEM = "src/image/item/scretItem.png";
+
+	/********************/
+	/********음식류********/
+	/********************/
+	/** 음식 : 사과*/
+	public static String FOOD_APPLE = "src/image/food/apple.png";
+	/** 음식 : 아보카도*/
+	public static String FOOD_AVOCADO = "src/image/food/avocado.png";
+	/** 음식 : 바나나*/
+	public static String FOOD_BANANA = "src/image/food/banana.png";
+	/** 음식 : 맥주*/
+	public static String FOOD_BEER = "src/image/food/beer.png";
+	/** 음식 : 체리*/
+	public static String FOOD_CHERRY = "src/image/food/cherry.png";
+	/** 음식 : 옥수수*/
+	public static String FOOD_CORN = "src/image/food/corn.png";
+	/** 음식 : 파인애플*/
+	public static String FOOD_FINEAPPLE = "src/image/food/fineapple.png";
+	/** 음식 : 레몬*/
+	public static String FOOD_LEMON = "src/image/food/lemon.png";
+	/** 음식 : 오렌지*/
+	public static String FOOD_ORANGE = "src/image/food/orange.png";
+	/** 음식 : 수박*/
+	public static String FOOD_WATERMELON = "src/image/food/watermelon.png";
+
+	/********************/
+	/********스테이지********/
+	/********************/
+	/** 스테이지 : 첫번째*/
+	public static String STAGE_FIRST = "";
+	/** 스테이지 : 두번째*/
+	public static String STAGE_SECOND = "";
+	/** 스테이지 : 세번째*/
+	public static String STAGE_THIRD = "";
+	/** 스테이지 : 네번째*/
+	public static String STAGE_FOURTH = "";
+	/** 스테이지 : 다섯번째*/
+	public static String STAGE_FIFTH = "";
+	/** 스테이지 : 여섯번째*/
+	public static String STAGE_SIXTH = "";
+	/** 스테이지 : 일곱번째*/
+	public static String STAGE_SEVENTH = "";
+	/** 스테이지 : 여덟번째*/
+	public static String STAGE_EIGHTH = "";
+	/** 스테이지 : 아홉번째*/
+	public static String STAGE_NINTH = "";
+	/** 스테이지 : 열번째*/
+	public static String STAGE_TENTH = "";
+
+}
